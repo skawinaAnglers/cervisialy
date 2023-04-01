4b8d6a65bc5afdd18f965f6feb2a5ffe8bdee9c6
magnuschase <jakub@kapala.xyz>
2023-04-01 21:54:07 +0200

    feat: add random names generator

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import { UserRecord } from 'firebase-admin/lib/auth/user-record'
 import User from './shared/User.interface'
+import { generateRandomName } from './helpers/generateRandomName'
 
 admin.initializeApp()
 
@@ -13,6 +14,7 @@ export const createUserProfile = functions.auth
 	.onCreate(async (user: UserRecord) => {
 		const userData = {
 			createDate: Date.now(),
+			name: generateRandomName()
 		} as User
 		const userDoc = db.collection('users').doc(user.uid)
 		await userDoc.set(userData)
--- functions/src/shared/User.interface.ts
+++ functions/src/shared/User.interface.ts
@@ -1,3 +1,4 @@
 export default interface User {
-	createDate: number
+	createDate: number,
+	name: string
 }
