e3a733f65b424a8063abd3d5e15920244f22a18a
magnuschase <jakub@kapala.xyz>
2023-04-01 13:39:13 +0200

    feat: add Login & Password inputs to LoginScreen

--- src/components/RegularTextInput.tsx
+++ src/components/RegularTextInput.tsx
@@ -4,22 +4,24 @@ import {
 	StyleProp,
 	ViewStyle,
 	NativeSyntheticEvent,
-	TextInputKeyPressEventData
+	TextInputKeyPressEventData,
+	TouchableOpacity
 } from 'react-native'
 import React, { useMemo } from 'react'
 import { SvgProps } from 'react-native-svg'
 import { useTailwind } from 'tailwind-rn'
+import VisibleIcon from '../assets/VisibleIcon'
+import NotVisibleIcon from '../assets/NotVisibleIcon'
 
 
-type TextInputPayload = {
+interface TextInputPayload {
 	placeholder?: string,
 	value: string,
 	onChangeText: (text: string) => void,
-	required?: boolean,
 	charLimit?: number,
 	icon?: React.FC<SvgProps>,
 	style?: StyleProp<ViewStyle>,
-	isInvalid?: boolean,
+	switchVisibility?: boolean,
 	onKeyPress?: (e: NativeSyntheticEvent<TextInputKeyPressEventData>) => void,
 	onPressOut?: () => void,
 	onSubmit?: () => void
@@ -28,16 +30,26 @@ const RegularTextInput: React.FC<TextInputPayload> = ({
 	placeholder,
 	value,
 	onChangeText,
-	required = false,
+	switchVisibility = false,
 	charLimit,
 	icon: IconComponent,
 	style,
-	isInvalid = false,
 	onKeyPress,
 	onPressOut,
 	onSubmit
 }) => {
 	const tailwind = useTailwind()
+	const [visible, setVisible] = React.useState<boolean>(false)
+
+	const VisiblityIcon: JSX.Element = useMemo(() => {
+		const props = {
+			width: 22,
+			height: 22,
+			fill: tailwind('text-neutral-400').color as string
+		}
+
+		return visible ? <VisibleIcon {...props} /> : <NotVisibleIcon {...props} />
+	}, [visible])
 
 	const onChange = (text: string) => {
 		if (charLimit && text.length > charLimit) {
@@ -46,13 +58,6 @@ const RegularTextInput: React.FC<TextInputPayload> = ({
 		}
 		onChangeText(text)
 	}
-
-	const isValid = useMemo(() => {
-		if (!required) return true
-		if (isInvalid) return false
-		if (value.length > 0) return true
-		return false
-	}, [isInvalid, value, required])
 	
 	return (
 		<View
@@ -63,19 +68,33 @@ const RegularTextInput: React.FC<TextInputPayload> = ({
 		>
 			
 			<View style={[tailwind('relative flex items-center justify-center w-full')]}>
+				
 				{
 					IconComponent && (
 						<View
 							style={[
-								tailwind('absolute right-0 mr-4 z-20 flex items-center justify-center'),
+								tailwind('absolute left-4 z-20 flex items-center justify-center'),
 							]}
 						>
 							<IconComponent
-								width={22}
-								height={22}
-								fill={tailwind(`text-${isValid ? 'slate' : 'red'}-500`).color as string}
+								width={24}
+								height={24}
+								fill={tailwind('text-neutral-400').color as string}
 							/>
 						</View>
+					)
+				}
+
+				{
+					switchVisibility && (
+						<TouchableOpacity
+							style={[
+								tailwind('absolute right-0 mr-4 z-20 flex items-center justify-center'),
+							]}
+							onPress={() => setVisible(!visible)}
+						>
+							{VisiblityIcon}
+						</TouchableOpacity>
 					)
 				}
 				
@@ -84,12 +103,14 @@ const RegularTextInput: React.FC<TextInputPayload> = ({
 					value={value}
 					onChangeText={onChange}
 					style={[
-						tailwind(`mt-1 border border-slate-800 font-light w-full rounded-md p-4 pr-12 text-slate-300 bg-slate-800`),
-						!isValid && tailwind('border-red-500'),
+						tailwind(`mt-1 border border-neutral-700 font-light w-full rounded-md p-4 text-neutral-100 bg-neutral-700`),
+						IconComponent && tailwind('pl-12'),
+						switchVisibility && tailwind('pr-12')
 					]}
+					secureTextEntry={switchVisibility && visible}
 					autoCapitalize='none'
 					returnKeyType='done'
-					placeholderTextColor={tailwind('text-slate-500').color as string}
+					placeholderTextColor={tailwind('text-neutral-100/70').color as string}
 					onSubmitEditing={onSubmit}
 					onKeyPress={onKeyPress}
 					onPressOut={onPressOut}
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -3,14 +3,34 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { View } from 'react-native'
 import { useTailwind } from 'tailwind-rn'
 import Quote from '../components/Quote'
+import RegularTextInput from '../components/RegularTextInput'
+import UserIcon from '../assets/UserIcon'
+import KeyIcon from '../assets/KeyIcon'
 
 const LoginScreen: React.FC = () => {
   const tailwind = useTailwind()
+	const [login, setLogin] = React.useState<string>('')
+	const [password, setPassword] = React.useState<string>('')
 
   return (
     <View style={[tailwind('px-6 pt-10 bg-neutral-900')]}>
       <SafeAreaView>
 				<Quote />
+				<RegularTextInput 
+					placeholder="Login"
+					style={[tailwind('mt-6')]}
+					value={login}
+					onChangeText={setLogin}
+					icon={UserIcon}
+				/>
+				<RegularTextInput 
+					placeholder="Password"
+					style={[tailwind('mt-4')]}
+					value={password}
+					switchVisibility
+					onChangeText={setPassword}
+					icon={KeyIcon}
+				/>
       </SafeAreaView>
     </View>
   )
